a7687434c7d4931c883de25e68bd632049c72693
Doniben <doniben@esperanto.co>
2019-09-06 17:21:58 -0500

    [FEATURE] Drawing a box animated with turtle + Comments

--- Python-1.py
+++ Python-1.py
@@ -1,7 +1,11 @@
+## Módulo de python para generar un dibujo
 import turtle
+# Generar una ventana
 window = turtle.Screen()
 david = turtle.Turtle()
+# Linea con distancia de 50
 david.forward(50)
+# Dar una vuelta de 90 grados
 david.left(90)
 david.forward(50)
 david.left(90)
@@ -9,5 +13,5 @@ david.forward(50)
 david.left(90)
 david.forward(50)
 david.left(90)
-
+# No cerrar la ventana
 turtle.mainloop()
