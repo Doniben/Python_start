c11c421133c866592ebe5c2011207402f6217862
Doniben <doniben@esperanto.co>
2019-09-06 18:50:03 -0500

    [FEATURE] drawing a star with a circle arround

---
+++ start.py
@@ -0,0 +1,19 @@
+import turtle
+from turtle import *
+color('red','green')
+window = turtle.Screen()
+star = turtle.Turtle()
+star.left(-108)
+star.forward(90)
+star.left(144)
+star.forward(90)
+star.left(144)
+star.forward(90)
+star.left(144)
+star.forward(90)
+star.left(144)
+star.forward(90)
+star.left(72)
+star.circle(45)
+
+turtle.mainloop()
