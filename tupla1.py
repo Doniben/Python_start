a5fa5b7d282b36cb993062c83a44cf3b2fa6e926
Doniben <doniben@esperanto.co>
2019-09-23 15:30:04 -0500

    [FEATURE] impresion de tuplas

---
+++ tupla1.py
@@ -0,0 +1,25 @@
+#Tupla de enteros
+
+tupla = (1,2,3,4,5)
+
+#tupla de cadenas
+
+tupla2 = ('caliente', 'frio', 'abierto', 'cerrado')
+
+#tupla con diferentes tipos de datos
+
+tupla3 = (1, 'caliente', True, 1.9)
+
+print(tupla)
+print(tupla2)
+print(tupla3)
+
+#imprimir tupla por secciones
+
+print (tupla3[1])
+
+#Recorrer tupla
+
+usuario = ('Doniben', 28, 'programador', 'Docente', 'guionista', 1991)
+for i in usuario:
+    print(i)
